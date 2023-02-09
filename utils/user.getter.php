19c09ed44a10a1db8e4ad26a24c50c7f6101c0e3
DoctorReaperClauss <99545836+DoctorReaperClauss@users.noreply.github.com>
2023-02-09 08:31:10 -0800

    Html basico del menu principal de niveles, anexion de clases dinamicas de cada nivel y funciones para obtener el progreso del jugador

--- utils/user.getter.php
+++ utils/user.getter.php
@@ -1,19 +1,27 @@
 <?php include_once "../config/database.php" ?>
 
 <?php
+$conexion = new DBOPERATION('../database/math.db');
 
 function id_usuario(string $user_name){
-    $conexion = new DBOPERATION('../database/math.db');
+    global $conexion;
     $user_id = $conexion->retornar_resultado_de_busqueda("SELECT user_id FROM users WHERE user_name=:user_name",
                                                         [$user_name]);
     return $user_id[0][0];
 }
 
 function puntaje_usuario($user_id){
-    $conexion = new DBOPERATION('../database/math.db');
+    global $conexion;
     $puntaje = $conexion->retornar_resultado_de_busqueda("SELECT puntuacion FROM puntaje WHERE user_id=:user_id",
                                                         [$user_id]);
     return $puntaje[0][0];
+}
+
+function progreso_niveles_usuario($user_id){
+    global $conexion;
+    $niveles = $conexion->retornar_resultado_de_busqueda("SELECT level_id, level_status FROM niveles WHERE user_id=:user_id",
+                                                        [$user_id]);
+    return $niveles;
 }
 
 ?>
--- views/main.php
+++ views/main.php
@@ -1,12 +1,22 @@
 <?php include_once "../utils/session.validator.php" ?>
 <?php include_once "../utils/user.getter.php" ?>
-<?php include_once "../config/database.php" ?>
 
 <?php
 $user_name = $_SESSION['user'];
-$conexion = new DBOPERATION('../database/math.db');
 $user_id = id_usuario($user_name);
 $user_score = puntaje_usuario($user_id);
+$user_levels = progreso_niveles_usuario($user_id);
+
+function asignar_clase($level_number){
+    global $user_levels;
+    $level_info = $user_levels[$level_number];
+
+    if($level_info['level_status'] == 1){ //nivel desbloqueado
+        return "desbloqueado";
+    }
+
+    return "bloqueado";
+}
 
 ?>
 
@@ -24,8 +34,44 @@ $user_score = puntaje_usuario($user_id);
 <body>
     <div class="container">
         <aside class="user-info">
-            <h2><?php echo $user_name; ?></h2>
+            <h2>Usuario</h2>
+            <p> <?php echo $user_name; ?> </p>
+            <h3>Puntaje</h3>
+            <p> <?php echo $user_score ?> </p>
         </aside>
+        <div class="main-menu">
+            <h1>Guerras Matematicas</h1>
+            <table>
+                <tr>
+                    <td class="nivel <?php echo asignar_clase(0) ?>">NIVEL 1</td>
+                    <td class="nivel <?php echo asignar_clase(1) ?>">NIVEL 2</td>
+                    <td class="nivel <?php echo asignar_clase(2) ?>">NIVEL 3</td>
+                    <td class="nivel <?php echo asignar_clase(3) ?>">NIVEL 4</td>
+                    <td class="nivel <?php echo asignar_clase(4) ?>">NIVEL 5</td>
+                </tr>
+                <tr>
+                    <td class="nivel <?php echo asignar_clase(5) ?>">NIVEL 6</td>
+                    <td class="nivel <?php echo asignar_clase(6) ?>">NIVEL 7</td>
+                    <td class="nivel <?php echo asignar_clase(7) ?>">NIVEL 8</td>
+                    <td class="nivel <?php echo asignar_clase(8) ?>">NIVEL 9</td>
+                    <td class="nivel <?php echo asignar_clase(9) ?>">NIVEL 10</td>
+                </tr>
+                <tr>
+                    <td class="nivel <?php echo asignar_clase(10) ?>">NIVEL 11</td>
+                    <td class="nivel <?php echo asignar_clase(11) ?>">NIVEL 12</td>
+                    <td class="nivel <?php echo asignar_clase(12) ?>">NIVEL 13</td>
+                    <td class="nivel <?php echo asignar_clase(13) ?>">NIVEL 14</td>
+                    <td class="nivel <?php echo asignar_clase(14) ?>">NIVEL 15</td>
+                </tr>
+                <tr>
+                    <td class="nivel <?php echo asignar_clase(15) ?>">NIVEL 16</td>
+                    <td class="nivel <?php echo asignar_clase(16) ?>">NIVEL 17</td>
+                    <td class="nivel <?php echo asignar_clase(17) ?>">NIVEL 18</td>
+                    <td class="nivel <?php echo asignar_clase(18) ?>">NIVEL 19</td>
+                    <td class="nivel <?php echo asignar_clase(19) ?>">NIVEL 20</td>
+                </tr>
+            </table>
+        </div>
     </div>
 </body>
 
