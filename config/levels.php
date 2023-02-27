84151aa288396085e63ac1082c6dfde86b875fca
DoctorReaperClauss <99545836+DoctorReaperClauss@users.noreply.github.com>
2023-02-27 10:18:23 -0800

    html de levels

--- config/levels.php
+++ config/levels.php
@@ -2,460 +2,460 @@
 
 $level_1 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "01+14",
+        "PROBLEMA" => "01+14",
         "RESULTADO" => "15"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "12+08",
+        "PROBLEMA" => "12+08",
         "RESULTADO" => "20"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "16+16",
+        "PROBLEMA" => "16+16",
         "RESULTADO" => "32"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "20+10",
+        "PROBLEMA" => "20+10",
         "RESULTADO" => "30"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "13+17",
+        "PROBLEMA" => "13+17",
         "RESULTADO" => "30"
     )
 );
 
 $level_2 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "40+20",
+        "PROBLEMA" => "40+20",
         "RESULTADO" => "60"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "28+22",
+        "PROBLEMA" => "28+22",
         "RESULTADO" => "50"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "32+18",
+        "PROBLEMA" => "32+18",
         "RESULTADO" => "50"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "25+25",
+        "PROBLEMA" => "25+25",
         "RESULTADO" => "50"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "30+70",
+        "PROBLEMA" => "30+70",
         "RESULTADO" => "100"
     )
 );
 
 $level_3 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "27+25",
+        "PROBLEMA" => "27+25",
         "RESULTADO" => "52"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "55+35",
+        "PROBLEMA" => "55+35",
         "RESULTADO" => "90"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "60+40",
+        "PROBLEMA" => "60+40",
         "RESULTADO" => "100"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "75+25",
+        "PROBLEMA" => "75+25",
         "RESULTADO" => "100"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "58+22",
+        "PROBLEMA" => "58+22",
         "RESULTADO" => "80"
     )
 );
 
 $level_4 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "43+57",
+        "PROBLEMA" => "43+57",
         "RESULTADO" => "100"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "49+38",
+        "PROBLEMA" => "49+38",
         "RESULTADO" => "87"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "34+56",
+        "PROBLEMA" => "34+56",
         "RESULTADO" => "90"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "38+62",
+        "PROBLEMA" => "38+62",
         "RESULTADO" => "100"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "24+48",
+        "PROBLEMA" => "24+48",
         "RESULTADO" => "72"
     )
 );
 
 $level_5 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "60+40",
+        "PROBLEMA" => "60+40",
         "RESULTADO" => "100"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "32+40",
+        "PROBLEMA" => "32+40",
         "RESULTADO" => "72"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "80+12",
+        "PROBLEMA" => "80+12",
         "RESULTADO" => "92"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "50+60",
+        "PROBLEMA" => "50+60",
         "RESULTADO" => "110"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "22+32",
+        "PROBLEMA" => "22+32",
         "RESULTADO" => "54"
     )
 );
 
 $level_6 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "20-9",
+        "PROBLEMA" => "20-9",
         "RESULTADO" => "11"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "46-18",
+        "PROBLEMA" => "46-18",
         "RESULTADO" => "28"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "25-12",
+        "PROBLEMA" => "25-12",
         "RESULTADO" => "13"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "32-40",
+        "PROBLEMA" => "32-40",
         "RESULTADO" => "-8"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "86-62",
+        "PROBLEMA" => "86-62",
         "RESULTADO" => "24"
     )
 );
 
 $level_7 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "46-42",
+        "PROBLEMA" => "46-42",
         "RESULTADO" => "4"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "82-16",
+        "PROBLEMA" => "82-16",
         "RESULTADO" => "66"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "37-20",
+        "PROBLEMA" => "37-20",
         "RESULTADO" => "17"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "120-60",
+        "PROBLEMA" => "120-60",
         "RESULTADO" => "60"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "85-18",
+        "PROBLEMA" => "85-18",
         "RESULTADO" => "67"
     )
 );
 
 $level_8 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "50-49",
+        "PROBLEMA" => "50-49",
         "RESULTADO" => "1"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "60-25",
+        "PROBLEMA" => "60-25",
         "RESULTADO" => "35"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "87-28",
+        "PROBLEMA" => "87-28",
         "RESULTADO" => "59"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "110-35",
+        "PROBLEMA" => "110-35",
         "RESULTADO" => "75"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "20-20",
+        "PROBLEMA" => "20-20",
         "RESULTADO" => "0"
     )
 );
 
 $level_9 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "92-21",
+        "PROBLEMA" => "92-21",
         "RESULTADO" => "71"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "70-40",
+        "PROBLEMA" => "70-40",
         "RESULTADO" => "30"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "62-39",
+        "PROBLEMA" => "62-39",
         "RESULTADO" => "23"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "18-12",
+        "PROBLEMA" => "18-12",
         "RESULTADO" => "6"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "53-40",
+        "PROBLEMA" => "53-40",
         "RESULTADO" => "13"
     )
 );
 
 $level_10 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "20-25",
+        "PROBLEMA" => "20-25",
         "RESULTADO" => "-5"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "60-29",
+        "PROBLEMA" => "60-29",
         "RESULTADO" => "31"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "86-42",
+        "PROBLEMA" => "86-42",
         "RESULTADO" => "44"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "92-59",
+        "PROBLEMA" => "92-59",
         "RESULTADO" => "33"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "78-19",
+        "PROBLEMA" => "78-19",
         "RESULTADO" => "59"
     )
 );
 
 $level_11 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "5x5",
+        "PROBLEMA" => "5x5",
         "RESULTADO" => "25"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "4x10",
+        "PROBLEMA" => "4x10",
         "RESULTADO" => "40"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "7x3",
+        "PROBLEMA" => "7x3",
         "RESULTADO" => "21"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "2x8",
+        "PROBLEMA" => "2x8",
         "RESULTADO" => "16"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "4x12",
+        "PROBLEMA" => "4x12",
         "RESULTADO" => "48"
     )
 );
 
 $level_12 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "20x4",
+        "PROBLEMA" => "20x4",
         "RESULTADO" => "80"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "8x8",
+        "PROBLEMA" => "8x8",
         "RESULTADO" => "64"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "7x7",
+        "PROBLEMA" => "7x7",
         "RESULTADO" => "49"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "25x3",
+        "PROBLEMA" => "25x3",
         "RESULTADO" => "75"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "3x8",
+        "PROBLEMA" => "3x8",
         "RESULTADO" => "24"
     )
 );
 
 $level_13 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "4x6",
+        "PROBLEMA" => "4x6",
         "RESULTADO" => "24"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "6x8",
+        "PROBLEMA" => "6x8",
         "RESULTADO" => "48"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "2x9",
+        "PROBLEMA" => "2x9",
         "RESULTADO" => "18"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "10x2",
+        "PROBLEMA" => "10x2",
         "RESULTADO" => "20"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "9x9",
+        "PROBLEMA" => "9x9",
         "RESULTADO" => "81"
     )
 );
 
 $level_14 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "8x2",
+        "PROBLEMA" => "8x2",
         "RESULTADO" => "16"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "20x3",
+        "PROBLEMA" => "20x3",
         "RESULTADO" => "60"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "15x3",
+        "PROBLEMA" => "15x3",
         "RESULTADO" => "45"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "5x9",
+        "PROBLEMA" => "5x9",
         "RESULTADO" => "45"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "5x10",
+        "PROBLEMA" => "5x10",
         "RESULTADO" => "50"
     )
 );
 
 $level_15 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "7x3",
+        "PROBLEMA" => "7x3",
         "RESULTADO" => "21"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "22x3",
+        "PROBLEMA" => "22x3",
         "RESULTADO" => "66"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "15x1",
+        "PROBLEMA" => "15x1",
         "RESULTADO" => "15"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "10x8",
+        "PROBLEMA" => "10x8",
         "RESULTADO" => "80"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "5x20",
+        "PROBLEMA" => "5x20",
         "RESULTADO" => "100"
     )
 );
 
 $level_16 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "12/2",
+        "PROBLEMA" => "12/2",
         "RESULTADO" => "6"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "20/5",
+        "PROBLEMA" => "20/5",
         "RESULTADO" => "4"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "18/3",
+        "PROBLEMA" => "18/3",
         "RESULTADO" => "6"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "55/5",
+        "PROBLEMA" => "55/5",
         "RESULTADO" => "11"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "80/2",
+        "PROBLEMA" => "80/2",
         "RESULTADO" => "40"
     )
 );
 
 $level_17 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "40/8",
+        "PROBLEMA" => "40/8",
         "RESULTADO" => "5"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "90/3",
+        "PROBLEMA" => "90/3",
         "RESULTADO" => "30"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "80/8",
+        "PROBLEMA" => "80/8",
         "RESULTADO" => "10"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "100/5",
+        "PROBLEMA" => "100/5",
         "RESULTADO" => "20"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "75/5",
+        "PROBLEMA" => "75/5",
         "RESULTADO" => "15"
     )
 );
 
 $level_18 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "30/10",
+        "PROBLEMA" => "30/10",
         "RESULTADO" => "3"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "80/4",
+        "PROBLEMA" => "80/4",
         "RESULTADO" => "20"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "40/4",
+        "PROBLEMA" => "40/4",
         "RESULTADO" => "10"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "68/4",
+        "PROBLEMA" => "68/4",
         "RESULTADO" => "17"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "95/5",
+        "PROBLEMA" => "95/5",
         "RESULTADO" => "19"
     )
 );
 
 $level_19 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "90/5",
+        "PROBLEMA" => "90/5",
         "RESULTADO" => "18"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "66/3",
+        "PROBLEMA" => "66/3",
         "RESULTADO" => "22"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "78/6",
+        "PROBLEMA" => "78/6",
         "RESULTADO" => "13"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "86/2",
+        "PROBLEMA" => "86/2",
         "RESULTADO" => "43"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "100/4",
+        "PROBLEMA" => "100/4",
         "RESULTADO" => "25"
     )
 );
 
 $level_20 = array(
     "EJERCICIO_1" => array(
-        "PPROBLEMA" => "20/4",
+        "PROBLEMA" => "20/4",
         "RESULTADO" => "5"
     ),
     "EJERCICIO_2" => array(
-        "PPROBLEMA" => "10/2",
+        "PROBLEMA" => "10/2",
         "RESULTADO" => "5"
     ),
     "EJERCICIO_3" => array(
-        "PPROBLEMA" => "50/10",
+        "PROBLEMA" => "50/10",
         "RESULTADO" => "5"
     ),
     "EJERCICIO_4" => array(
-        "PPROBLEMA" => "30/2",
+        "PROBLEMA" => "30/2",
         "RESULTADO" => "15"
     ),
     "EJERCICIO_5" => array(
-        "PPROBLEMA" => "100/2",
+        "PROBLEMA" => "100/2",
         "RESULTADO" => "50"
     )
 );
--- views/level.php
+++ views/level.php
@@ -4,10 +4,6 @@
 <?php
 $level = get_levels($_SESSION['level_id']);
 
-if($_POST){
-    print_r($_POST);
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -16,11 +12,40 @@ if($_POST){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/styles/level.css">
     <title>Document</title>
 </head>
 <body>
-    <form action="level.php", method="post">
-        <input class="btn-pista" type="submit" value="Pista" name="pista">
+    <form class="float-form" action="level.php", method="post">
+        <input class="btn-pista" type="submit" value="Canjear Pista" name="pista">
+    </form>
+    <form class="main-form" action="level.php", method="post">
+        <div class="card-ejercicio">
+            <h2>Ejercicio 1</h2>
+            <p><?php echo $level['EJERCICIO_1']["PROBLEMA"]; ?></p>
+            <input class="result" type="text" name="resultado-1" placeholder="Coloque El Resultado">
+        </div>
+        <div class="card-ejercicio">
+            <h2>Ejercicio 2</h2>
+            <p><?php echo $level['EJERCICIO_2']["PROBLEMA"]; ?></p>
+            <input class="result" type="text" name="resultado-2" placeholder="Coloque El Resultado">
+        </div>
+        <div class="card-ejercicio">
+            <h2>Ejercicio 3</h2>
+            <p><?php echo $level['EJERCICIO_3']["PROBLEMA"]; ?></p>
+            <input class="result" type="text" name="resultado-3" placeholder="Coloque El Resultado">
+        </div>
+        <div class="card-ejercicio">
+            <h2>Ejercicio 4</h2>
+            <p><?php echo $level['EJERCICIO_4']["PROBLEMA"]; ?></p>
+            <input class="result" type="text" name="resultado-4" placeholder="Coloque El Resultado">
+        </div>
+        <div class="card-ejercicio">
+            <h2>Ejercicio 5</h2>
+            <p><?php echo $level['EJERCICIO_5']["PROBLEMA"]; ?></p>
+            <input class="result" type="text" name="resultado-5" placeholder="Coloque El Resultado">
+        </div>
+        <input class="btn-result" type="submit" name="submit-resultado" value="Validar Resultados">
     </form>
 
 </body>
