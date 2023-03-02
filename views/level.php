becd7f2a91511ac99f8f63b258667f1306c75598
DoctorReaperClauss <99545836+DoctorReaperClauss@users.noreply.github.com>
2023-03-02 09:11:14 -0800

    Ligera mejora a la legibilidad del codigo

--- views/level.php
+++ views/level.php
@@ -3,6 +3,11 @@
 
 <?php
 $level = get_levels($_SESSION['level_id']);
+$resultado_1 = '';
+$resultado_2 = '';
+$resultado_3 = '';
+$resultado_4 = '';
+$resultado_5 = '';
 $error = array();
 
 function inputs_vacios($array){
@@ -28,6 +33,15 @@ function inputs_no_numericos($array){
 
 if($_POST){
     if($_POST['form-control'] == 'Validar Resultados'){
+        //asignar valores a cada variable para poder mostrarselas al usuario
+        //y que no tenga que reescribir todo una y otra vez
+        $resultado_1 = $_POST['resultado-1'];
+        $resultado_2 = $_POST['resultado-2'];
+        $resultado_3 = $_POST['resultado-3'];
+        $resultado_4 = $_POST['resultado-4'];
+        $resultado_5 = $_POST['resultado-5'];
+
+        //validaciones
         if(inputs_vacios($_POST)){
             array_push($error, "Hay ejercicios sin resolver, por favor resuelvalos todos antes");
         }
@@ -39,8 +53,8 @@ if($_POST){
             //codigo para validar los resultados
             print_r("hola");
         }
-    }else{
-
+    }elseif($_POST['form-control'] == 'Canjear Pista'){
+        //falta por implementar la logica de las pistas
     }
 }
 
@@ -73,27 +87,27 @@ if($_POST){
         <div class="card-ejercicio">
             <h2>Ejercicio 1</h2>
             <p><?php echo $level['EJERCICIO_1']["PROBLEMA"]; ?></p>
-            <input class="result" type="text" name="resultado-1" placeholder="Coloque El Resultado" value="<?php if($_POST){echo $_POST['resultado-1'];} ?>" required>
+            <input class="result" type="text" name="resultado-1" placeholder="Coloque El Resultado" value="<?php echo $resultado_1; ?>" required>
         </div>
         <div class="card-ejercicio">
             <h2>Ejercicio 2</h2>
             <p><?php echo $level['EJERCICIO_2']["PROBLEMA"]; ?></p>
-            <input class="result" type="text" name="resultado-2" placeholder="Coloque El Resultado" value="<?php if($_POST){echo $_POST['resultado-2'];} ?>" required>
+            <input class="result" type="text" name="resultado-2" placeholder="Coloque El Resultado" value="<?php echo $resultado_2; ?>" required>
         </div>
         <div class="card-ejercicio">
             <h2>Ejercicio 3</h2>
             <p><?php echo $level['EJERCICIO_3']["PROBLEMA"]; ?></p>
-            <input class="result" type="text" name="resultado-3" placeholder="Coloque El Resultado" value="<?php if($_POST){echo $_POST['resultado-3'];} ?>" required>
+            <input class="result" type="text" name="resultado-3" placeholder="Coloque El Resultado" value="<?php echo $resultado_3; ?>" required>
         </div>
         <div class="card-ejercicio">
             <h2>Ejercicio 4</h2>
             <p><?php echo $level['EJERCICIO_4']["PROBLEMA"]; ?></p>
-            <input class="result" type="text" name="resultado-4" placeholder="Coloque El Resultado" value="<?php if($_POST){echo $_POST['resultado-4'];} ?>" required>
+            <input class="result" type="text" name="resultado-4" placeholder="Coloque El Resultado" value="<?php echo $resultado_4; ?>" required>
         </div>
         <div class="card-ejercicio">
             <h2>Ejercicio 5</h2>
             <p><?php echo $level['EJERCICIO_5']["PROBLEMA"]; ?></p>
-            <input class="result" type="text" name="resultado-5" placeholder="Coloque El Resultado" value="<?php if($_POST){echo $_POST['resultado-5'];} ?>" required>
+            <input class="result" type="text" name="resultado-5" placeholder="Coloque El Resultado" value="<?php echo $resultado_5; ?>" required>
         </div>
         <input class="btn-result" type="submit" name="form-control" value="Validar Resultados">
     </form>
